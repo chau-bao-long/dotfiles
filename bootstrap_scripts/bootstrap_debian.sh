2e12c97208003eeb8695ae70b368549003deff36
chau.bao.long <chau.bao.long@framgia.com>
2019-05-17 00:03:21 +0700

    Bootstrap zsh autosuggestion.

--- bootstrap_scripts/bootstrap_debian.sh
+++ bootstrap_scripts/bootstrap_debian.sh
@@ -171,6 +171,7 @@ install_zsh() {
     sudo apt install zsh
     sudo chsh -s $(which zsh)
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     success "Installed zsh"
   fi
 }
--- bootstrap_scripts/bootstrap_redhat.sh
+++ bootstrap_scripts/bootstrap_redhat.sh
@@ -169,6 +169,7 @@ install_zsh() {
     sudo yum install -y zsh
     sudo chsh -s $(which zsh) root
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     success "Installed zsh"
   fi
 }
