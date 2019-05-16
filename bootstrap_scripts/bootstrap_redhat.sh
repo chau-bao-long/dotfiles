528938eca7285d39d7223ef3adbfc8c431747b36
chau.bao.long <chau.bao.long@framgia.com>
2019-05-16 22:25:38 +0700

    Bootstrap script for redhat.

--- bootstrap_scripts/bootstrap_redhat.sh
+++ bootstrap_scripts/bootstrap_redhat.sh
@@ -1,2 +1,185 @@
 #! /bin/bash
-echo "Hi from red hat"
+echo "---" 
+
+DOTFILES_ROOT=$(pwd -P)
+set -e
+
+info () {
+  printf "\r  [\033[00;34m..\033[0m] $1\n"
+}
+
+user () {
+  printf "\r  [\033[0;33m??\033[0m] $1\n"
+}
+
+success () {
+  printf "\r\033[2K [\033[00;32mOK\033[0m] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ] && [ ! -d $1 ] # "false" or empty
+  then
+    if [[ ! $1 =~ ^\.\/\..*\/.* ]] || [[ $1 =~ ^\.\/\..*\/.*conf.* ]]
+    then
+      local src="$DOTFILES_ROOT/$(echo $1 | cut -d '/' -f 2,3,4)"
+      ln -s $src "$2"
+      success "linked $src to $2"
+    else
+      cp "$1" "$2"
+      success "copied file $1 to $2"
+    fi
+  elif [ "$skip" != "true" ]
+  then
+    cp -r "$1" "$2"
+    success "copied directory $1 to $2"
+  fi
+}
+
+copy_dotfiles() {
+  info 'copying dotfiles to system'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  for src in $(find . -maxdepth 3 -regextype sed -regex ".*\(conf.*\|\.\/\..*\)" -not -path '*.git*')
+  do
+    dst="$HOME/$(echo $src | cut -d '/' -f 2,3,4)"
+    link_file "$src" "$dst"
+  done
+}
+
+install_nvim() {
+  read -r -p "Do you want to install neovim? [y|N] " response
+  if [[ $response =~ (y|yes|Y) ]];then
+    info "Installing neovim"
+    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum install -y neovim python{2,3}-neovim
+    success "Installed neovim"
+  fi
+}
+
+setup_nvim() {
+  read -r -p "Do you want to install vim plugins now? [y|N] " response
+  if [[ $response =~ (y|yes|Y) ]];then
+    info "Installing vim plugins"
+    nvim +PlugInstall +qall > /dev/null 2>&1
+    success "Installed vim plugins"
+  else
+    success "skipped. Install by running :PlugInstall within vim"
+  fi
+}
+
+install_tmux() {
+  read -r -p "Do you want to install tmux? [y|N] " response
+  if [[ $response =~ (y|yes|Y) ]];then
+    info "Installing tmux"
+    sudo yum install -y tmux
+    success "Installed tmux"
+  fi
+}
+
+install_ranger() {
+  read -r -p "Do you want to install ranger? [y|N] " response
+  if [[ $response =~ (y|yes|Y) ]];then
+    info "Installing ranger"
+    git clone https://github.com/ranger/ranger.git
+    cd ranger
+    sudo make install
+    cd ..
+    sudo rm -rf ranger
+    success "Installed ranger"
+  fi
+}
+
+install_zsh() {
+  read -r -p "Do you want to install zsh? [y|N] " response
+  if [[ $response =~ (y|yes|Y) ]];then
+    sudo yum install zsh
+    chsh -s $(which zsh) root
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    success "Installed zsh"
+  fi
+}
+
+install_nvim
+install_ranger
+install_tmux
+install_zsh
+copy_dotfiles
+setup_nvim
+
+echo "---" 
+
+echo "Finish Install!"
