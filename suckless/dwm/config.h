5782bd1408395c68c169b0cc8e5e15366741585f
Chau Bao Long <chau.bao.long.vn@gmail.com>
2019-06-09 16:35:28 +0700

    Update dwm dmenu color to green style.

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *emojicmd[]  = { "get-emoji", NULL };
 
