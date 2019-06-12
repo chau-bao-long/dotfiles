09f7b8d0f020a7706452eb950c87da4ce1164abe
Chau Bao Long· <chau.bao.long.vn@gmail.com>
2019-06-12 22:48:15 +0700

    Fix bug when install nvim and softwares.

--- bootstrap_scripts/arch/install_nvim.sh
+++ bootstrap_scripts/arch/install_nvim.sh
@@ -2,3 +2,4 @@
 
 sudo pacman -S --noconfirm neovim
 sudo pacman -S --noconfirm python python-pip
+pip3 install --user neovim
--- bootstrap_scripts/arch/install_softwares.sh
+++ bootstrap_scripts/arch/install_softwares.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo pacman -S --noconfirm qutebrowser zathura zathura-pdf-mupdf sxiv
-sudo yaourt -S s brave-bin telegram-desktop
+yaourt -S s brave-bin telegram-desktop
