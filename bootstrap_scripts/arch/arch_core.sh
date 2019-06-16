a8c9404dac158e3a396584734a599454bc4d206e
Chau Bao Long· <chau.bao.long.vn@gmail.com>
2019-06-16 14:50:54 +0700

    Add bootstrap script for bluetooth and sound.

--- bootstrap_scripts/arch/arch_core.sh
+++ bootstrap_scripts/arch/arch_core.sh
@@ -12,3 +12,9 @@ sudo pacman -S --noconfirm alsa-utils libnotify dunst feh dnsutils openssh
 sudo systemctl enable ntpd
 sudo systemctl start ntpd
 sudo timedatectl set-ntp on
+# bluetooth
+sudo pacman -S --noconfirm bluez bluez-utils
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+# sound
+sudo pacman -S --noconfirm pulseaudio pulseaudio-bluetooth pulseaudio-alsa pulsemixer
