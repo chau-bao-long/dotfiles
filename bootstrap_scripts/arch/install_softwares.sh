68c2c3bf29673ec42efa73a53f97a7b2ffee9319
Chau Bao Long· <chau.bao.long.vn@gmail.com>
2019-06-17 13:56:28 +0700

    Add media software player in bootstrap script.

--- bootstrap_scripts/arch/install_softwares.sh
+++ bootstrap_scripts/arch/install_softwares.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm qutebrowser zathura zathura-pdf-mupdf sxiv
+sudo pacman -S --noconfirm qutebrowser zathura zathura-pdf-mupdf sxiv cmus mpv
 yaourt -S s brave-bin telegram-desktop
