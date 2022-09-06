b8406dba386c7822c2fdc19886675253a88b77e4
long.chau.ext <long.chau.ext@personio.de>
2022-09-06 18:14:23 +0700

    Remove animation of neoscroll.

--- .vim/after/plugin/lua/scroll.lua
+++ .vim/after/plugin/lua/scroll.lua
@@ -3,14 +3,14 @@ require('neoscroll').setup{
 }
 
 local t = {}
-t['<c-k>'] = {'scroll', {'-vim.wo.scroll', 'true', '150'}}
-t['<c-j>'] = {'scroll', { 'vim.wo.scroll', 'true', '150'}}
-t['<c-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
-t['<c-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}}
-t['<c-l>'] = {'scroll', {'-0.10', 'false', '100'}}
-t['<backspace>'] = {'scroll', { '0.10', 'false', '100'}}
-t['zt']    = {'zt', {'150'}}
-t['zz']    = {'zz', {'150'}}
-t['zb']    = {'zb', {'150'}}
+t['<c-k>'] = {'scroll', {'-vim.wo.scroll', 'true', '0'}}
+t['<c-j>'] = {'scroll', { 'vim.wo.scroll', 'true', '0'}}
+t['<c-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '0'}}
+t['<c-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '0'}}
+t['<c-l>'] = {'scroll', {'-0.10', 'false', '0'}}
+t['<backspace>'] = {'scroll', { '0.10', 'false', '0'}}
+t['zt']    = {'zt', {'0'}}
+t['zz']    = {'zz', {'0'}}
+t['zb']    = {'zb', {'0'}}
 
 require('neoscroll.config').set_mappings(t)
