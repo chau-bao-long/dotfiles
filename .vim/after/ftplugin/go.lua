62b4d0e97f7a33439711ed2c23af57fe9c1264f5
long.chau.ext <long.chau.ext@personio.de>
2024-10-08 10:12:04 +0700

    Update go config.

--- .vim/after/ftplugin/go.lua
+++ .vim/after/ftplugin/go.lua
@@ -1,19 +1,13 @@
 require('go').setup({
   goimport='gofumports', -- goimport command
   gofmt = 'gofumpt', --gofmt cmd,
-  max_line_len = 120, -- max line length in goline format
   tag_transform = false, -- tag_transfer  check gomodifytags for details
   test_template = '', -- default to testify if not set; g:go_nvim_tests_template  check gotests for details
   test_template_dir = '', -- default to nil if not set; g:go_nvim_tests_template_dir  check gotests for details
   comment_placeholder = '' ,  -- comment_placeholder your cool placeholder e.g. ﳑ       
   verbose = false,  -- output loginf in messages
-  lsp_cfg = false, -- true: apply go.nvim non-default gopls setup
   lsp_gofumpt = false, -- true: set default gofmt in gopls format to gofumpt
-  lsp_on_attach = true, -- if a on_attach function provided:  attach on_attach function to gopls
-                       -- true: will use go.nvim on_attach if true
-                       -- nil/false do nothing
   gopls_cmd = {"gopls"},
-  lsp_diag_hdlr = true, -- hook lsp diag handler
   dap_debug = true, -- set to true to enable dap
   dap_debug_keymap = true, -- set keymaps for debugger
   dap_debug_gui = true, -- set to true to enable dap gui, highly recommand
